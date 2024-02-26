45f4373b25e96a910f7c6087672d8303d7889ea5
DDixon0 <dixon.denzell@gmail.com>
2024-02-25 22:11:38 -0500

    Initialized first audio cpp project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// vcpkg install audiofile
+// ./../../src/vcpkg/vcpkg install audiofile
+#include "AudioFile.h"
+
+
+
+int main(int, char**){
+    std::cout << "Hello, from soundTest!\n";
+
+    AudioFile<double> audioFile;
+}
